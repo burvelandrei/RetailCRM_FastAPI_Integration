2093af511a8898d589aa9aab65a00008c8d5920c
Andrei Burvel <burvelandrei@gmail.com>
2025-04-23 12:21:34 +0300

    feat: creating schemas customer

---
+++ schemas/customer.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from typing import Dict, List
+
+class Customer(BaseModel):
+    class Config:
+        extra = "allow"
+
+    id: int
+    firstName: str | None = None
+    email: str| None = None
+    createdAt: str | None = None
+
+class CustomersResponse(BaseModel):
+    success: bool
+    customers: List[Customer]
+    pagination: Dict | None = None
+
+class CreateCustomer(BaseModel):
+    firstName: str
+    email: str | None = None
+    phone: str | None = None
+    lastName: str | None = None
+    patronymic: str | None = None
+    externalId: str | None = None
+
+class CreateCustomerRequest(BaseModel):
+    site: str
+    customer: CreateCustomer
+
+class CreateCustomerResponse(BaseModel):
+    success: bool
+    id: int | None = None
+    errorMsg: str | None = None
